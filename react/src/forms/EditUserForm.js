1b464e3d75570c7ac27dec85465e153baefb4547
NongTran <ttnong@vn.isb.co.jp>
2019-11-12 13:08:26 +0700

    CRUD with Redux

--- react/src/forms/EditUserForm.js
+++ react/src/forms/EditUserForm.js
@@ -8,13 +8,6 @@ class EditUserForm extends React.Component{
 		}
   }
   
-  static getDerivedStateFromProps(props, state) {
-    return {
-      ...state,
-      user: props.currentUser
-    }
-  }
-
   // You can tell React to skip applying an effect if certain values haven’t changed between re-renders. [ props ]
 
   handleInputChange = event => {
@@ -26,18 +19,19 @@ class EditUserForm extends React.Component{
   }
 
   render() {
+    const {user} = this.state
     return(
     <form
       onSubmit={event => {
         event.preventDefault()
 
-        this.props.onUpdateUser(this.state.user.id, this.state.user)
+        this.props.onUpdateUser(user.id, user)
       }}
     >
       <label>Name</label>
-      <input type="text" name="name" value={this.state.user.name} onChange={this.handleInputChange} />
+      <input type="text" name="name" value={user.name} onChange={this.handleInputChange} />
       <label>Username</label>
-      <input type="text" name="username" value={this.state.user.username} onChange={this.handleInputChange} />
+      <input type="text" name="username" value={user.username} onChange={this.handleInputChange} />
       <button>Update user</button>
       <button onClick={() => this.props.onCancelEditing} className="button muted-button">
         Cancel
